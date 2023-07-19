556777bc55241d81be148c859f8691293e044b9a
Domagoj Šimatović <dosimatovic@gmail.com>
2023-07-19 21:17:17 +0200

    Input dodan

--- src/App.js
+++ src/App.js
@@ -1,52 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import User from "./components/User";
 import UserWithChildren from "./components/UserWithChildren";
 import UserClass from "./components/UserClass";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      korisnici: [
-        { ime: "Korisnik 1", godine: 25 },
-        { ime: "Korisnik 2", godine: 30 },
-        { ime: "Korisnik 3", godine: 35 },
-      ],
-      proizvoljnaVrijednost: "Proizvoljna vrijednost iz stanja",
-    };
-  }
+const App = () => {
+  const [korisnici, setKorisnici] = useState([
+    { ime: "Korisnik 1", godine: 25 },
+    { ime: "Korisnik 2", godine: 30 },
+    { ime: "Korisnik 3", godine: 35 },
+  ]);
+  const [proizvoljnaVrijednost, setProizvoljnaVrijednost] = useState(
+    "Proizvoljna vrijednost iz stanja"
+  );
 
-  promijeniGodine = () => {
-    this.setState({
-      korisnici: this.state.korisnici.map((korisnik) => ({
-        ...korisnik,
-        godine: korisnik.godine + 1,
-      })),
+  const promijeniGodineKorisnik1 = () => {
+    setKorisnici((prevKorisnici) => {
+      const updatedKorisnici = [...prevKorisnici];
+      updatedKorisnici[0] = {
+        ...updatedKorisnici[0],
+        godine: updatedKorisnici[0].godine + 1,
+      };
+      return updatedKorisnici;
     });
   };
 
-  render() {
-    return (
-      <div>
-        <h1>React Aplikacija</h1>
-        <UserClass
-          ime={this.state.korisnici[0].ime}
-          godine={this.state.korisnici[0].godine}
-        />
-        <User
-          ime={this.state.korisnici[1].ime}
-          godine={this.state.korisnici[1].godine}
-        />
-        <UserWithChildren
-          ime={this.state.korisnici[2].ime}
-          godine={this.state.korisnici[2].godine}
-        >
-          <p>{this.state.proizvoljnaVrijednost}</p>
-        </UserWithChildren>
-        <button onClick={this.promijeniGodine}>Promijeni godinu</button>
-      </div>
-    );
-  }
-}
+  const promijeniGodineKorisnik2 = () => {
+    setKorisnici((prevKorisnici) => {
+      const updatedKorisnici = [...prevKorisnici];
+      updatedKorisnici[1] = {
+        ...updatedKorisnici[1],
+        godine: updatedKorisnici[1].godine + 1,
+      };
+      return updatedKorisnici;
+    });
+  };
+
+  const promijeniGodineKorisnik3 = () => {
+    setKorisnici((prevKorisnici) => {
+      const updatedKorisnici = [...prevKorisnici];
+      updatedKorisnici[2] = {
+        ...updatedKorisnici[2],
+        godine: updatedKorisnici[2].godine + 1,
+      };
+      return updatedKorisnici;
+    });
+  };
+
+  const promijeniImeKorisnik1 = (newIme) => {
+    setKorisnici((prevKorisnici) => {
+      const updatedKorisnici = [...prevKorisnici];
+      updatedKorisnici[0] = {
+        ...updatedKorisnici[0],
+        ime: newIme,
+      };
+      return updatedKorisnici;
+    });
+  };
+
+  return (
+    <div>
+      <h1>React Aplikacija</h1>
+      <UserClass ime={korisnici[0].ime} godine={korisnici[0].godine} />
+      <User ime={korisnici[1].ime} godine={korisnici[1].godine} />
+      <UserWithChildren ime={korisnici[2].ime} godine={korisnici[2].godine}>
+        <p>{proizvoljnaVrijednost}</p>
+      </UserWithChildren>
+      <button onClick={promijeniGodineKorisnik1}>Promijeni godinu</button>
+      <button onClick={promijeniGodineKorisnik2}>Promijeni godinu</button>
+      <button onClick={promijeniGodineKorisnik3}>Promijeni godinu</button>
+      <input
+        type="text"
+        value={korisnici[0].ime}
+        onChange={(e) => promijeniImeKorisnik1(e.target.value)}
+      />
+    </div>
+  );
+};
 
 export default App;
