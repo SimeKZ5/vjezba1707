ce83af12477ebb6dee067f36287dbdfaa3afd0ce
Domagoj Šimatović <dosimatovic@gmail.com>
2023-07-19 18:23:28 +0200

    Dodan gumb

--- src/App.js
+++ src/App.js
@@ -2,22 +2,48 @@ import React from "react";
 import User from "./components/User";
 import UserWithChildren from "./components/UserWithChildren";
 import UserClass from "./components/UserClass";
-class App extends React.Component {
-  render() {
-    const korisnici = [
-      { ime: "Korisnik 1", godine: 25 },
-      { ime: "Korisnik 2", godine: 30 },
-      { ime: "Korisnik 3", godine: 35 },
-    ];
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      korisnici: [
+        { ime: "Korisnik 1", godine: 25 },
+        { ime: "Korisnik 2", godine: 30 },
+        { ime: "Korisnik 3", godine: 35 },
+      ],
+      proizvoljnaVrijednost: "Proizvoljna vrijednost iz stanja",
+    };
+  }
+
+  promijeniGodine = () => {
+    this.setState({
+      korisnici: this.state.korisnici.map((korisnik) => ({
+        ...korisnik,
+        godine: korisnik.godine + 1,
+      })),
+    });
+  };
+
+  render() {
     return (
       <div>
         <h1>React Aplikacija</h1>
-        <UserClass ime={korisnici[0].ime} godine={korisnici[0].godine} />
-        <User ime={korisnici[1].ime} godine={korisnici[1].godine} />
-        <UserWithChildren ime={korisnici[2].ime} godine={korisnici[2].godine}>
-          <p>Dodatne informacije o korisniku...</p>
+        <UserClass
+          ime={this.state.korisnici[0].ime}
+          godine={this.state.korisnici[0].godine}
+        />
+        <User
+          ime={this.state.korisnici[1].ime}
+          godine={this.state.korisnici[1].godine}
+        />
+        <UserWithChildren
+          ime={this.state.korisnici[2].ime}
+          godine={this.state.korisnici[2].godine}
+        >
+          <p>{this.state.proizvoljnaVrijednost}</p>
         </UserWithChildren>
+        <button onClick={this.promijeniGodine}>Promijeni godinu</button>
       </div>
     );
   }
